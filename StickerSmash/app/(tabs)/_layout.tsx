272c3335a5d1efec41f347d111c3ad2e71a95572
NICOMEKO <nicotatis789@gmail.com>
2026-05-05 18:29:00 -0500

    Add files via upload

---
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#65b5f7",
+        headerStyle: {
+          backgroundColor: "#c994f7",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#100c0c",
+        tabBarStyle: {
+          backgroundColor: "#aee3f1",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
